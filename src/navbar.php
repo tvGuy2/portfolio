f0a169ef71a1f2d754477978f4f95bc209860bde
Tanguy Vallot <tanguyvallot39@gmail.com>
2022-10-20 14:36:40 +0200

    premier commit

---
+++ src/navbar.php
@@ -0,0 +1,49 @@
+
+<nav class="navbar">
+        <ul class="navbar-menu">
+          <li class="navbar-item logo">
+            <a href="#" class="navbar-link">
+              <span class="navbar-title">Tanguy Vallot</span>
+              <i class="fas fa-chevron-right navbar-icon"></i>
+            </a>
+          </li>
+          <li class="navbar-item">
+            <a href="./vue/index.php" class="navbar-link">
+              <i class="fas fa-home navbar-icon"></i>
+              <span class="navbar-title">Accueil</span>
+            </a>
+          </li>
+          <li class="navbar-item">
+            <a href="#" class="navbar-link">
+              <i class="fas fa-folder navbar-icon"></i>
+              <span class="navbar-title">Projets</span>
+            </a>
+          </li>
+          <li class="navbar-item">
+            <a href="#" class="navbar-link">
+              <i class="fas fa-graduation-cap navbar-icon"></i>
+              <span class="navbar-title">Formation</span>
+            </a>
+          </li>
+          <li class="navbar-item">
+            <a href="/CV_Vallot_Tanguy.pdf" class="navbar-link">
+              <i class="fas fa-user navbar-icon"></i>
+              <span class="navbar-title">Mon CV</span>
+            </a>
+          </li>
+          <li class="navbar-item">
+            <a href="contact.html" class="navbar-link">
+              <i class="fas fa-phone navbar-icon"></i>
+              <span class="navbar-title">Contact</span>
+            </a>
+          </li>
+          <li class="navbar-item">
+            <a href="#" class="navbar-link">
+              <i class="fas fa-sign-out-alt navbar-icon"></i>
+              <span class="navbar-title">Exit</span>
+            </a>
+          </li>
+            
+            
+        </ul>
+      </nav>
